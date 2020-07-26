274dba475832808cb627238e7b2ecade533fc185
Sam <samyajyotibordoloi@gmail.com>
2020-07-26 20:23:50 +0530

    first commit

---
+++ removepunctuations/removepunctuations/urls.py
@@ -0,0 +1,37 @@
+"""web2 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+#from django.conf.urls import url
+#from django.contrib import admin
+
+#urlpatterns = [
+ #   url(r'^admin/', admin.site.urls),
+#]
+
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('analyze/', views.analyze, name='analyze'),
+    #path('capitalizefirst/', views.capfirst, name='capfirst'),
+    #path('newlineremove/', views.newlineremove, name='newlineremove'),
+   # path('spaceremove/', views.spaceremove, name='spaceremove'),
+   # path('charcount/', views.charcount, name='charcount'),
+
+]
---
+++ removepunctuations/removepunctuations/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def analyze(request):
+    # Get the text
+    djtext = request.GET.get('text', 'default')
+    removepunc=request.GET.get('removepunc','off')
+
+    if removepunc == "on":
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
+        params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+
+    else:
+        return HttpResponse('you have not click the checkbox')
